0dfeabe311aa5b6d22a1c862e294b2e6eba6b8a6
[firstname lastname] <[valid-email]>
2022-08-17 15:41:12 +0530

    explicit java script

---
+++ explicit.js
@@ -0,0 +1,12 @@
+var x = 120;
+var y = 123;
+if (x==y) {
+console.log('both are equal')
+}
+else{
+console.log('both are not equal')
+}
+var z=x+y;
+var a=x-y;
+var b=number(y);
+console.log(typeof b)
